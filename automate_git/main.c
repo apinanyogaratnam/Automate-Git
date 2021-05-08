8475d1497313ec09689b3462827e7b10bc3f4a20
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-08 16:04:56 -0400

    added comments

--- automate_git/main.c
+++ automate_git/main.c
@@ -7,14 +7,23 @@ int main() {
     bool init = false;
     if (init) system("git init");
 
+    // adding all changes
     system("git add .");
     char message[1024];
+
+    // get message from user
     printf("Enter commit message: ");
     fgets(message ,1024, stdin);
+
+    // remove '\n' from fgets()
     message[strcspn(message, "\n")] = 0;
+
+    // concatenating string for command
     strcat(message, "'");
     char command[1024] = "git commit -m '";
     strcat(command, message);
+
+    // commiting and pushing to github and clearing terminal output
     system(command);
     system("git push -u origin main");
     system("clear");
