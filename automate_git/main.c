d0b864ef5a0e7fee2dd27fca45db4965adcf646d
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-08 15:34:48 -0400

    created basic structure with testing

---
+++ automate_git/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+    bool init = false;
+    if (init) system("git init");
+    //system("git add .");
+    char message[1024];
+    fgets(message ,1024, stdin);
+    strcat(message, "'");
+    char command[1024] = "git commit -m '";
+    strcat(command, message);
+    // system("git commit -m '");
+    printf("%s\n", command);
+    return 0;
+}
