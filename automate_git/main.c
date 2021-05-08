a142f146461d3065848d43388422cca16cbaff37
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-08 15:42:18 -0400

    testing the message for commit running the automator

--- automate_git/main.c
+++ automate_git/main.c
@@ -6,14 +6,14 @@
 int main() {
     bool init = false;
     if (init) system("git init");
-    //system("git add .");
+    system("git add .");
     char message[1024];
     fgets(message ,1024, stdin);
     message[strcspn(message, "\n")] = 0;
     strcat(message, "'");
     char command[1024] = "git commit -m '";
     strcat(command, message);
-    // system("git commit -m '");
-    printf("%s\n", command);
+    system(command);
+    //printf("%s\n", command);
     return 0;
 }
