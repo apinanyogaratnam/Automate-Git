529091fd52979d4131d76c928c58b2f384a31753
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-09 17:06:02 -0400

    manually commiting with a.out to see how to remove a.out

---
+++ side_projects_computer_science/Completed/automate_git/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// api to create a new repo
+// curl -u "apinanyogaratnam" https://api.github.com/user/repos -d '{"name":"repo name", "private": true}'
+// curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
+
+// curl --location --request POST 'https://api.github.com/user/repos' \
+// --header 'Content-Type: application/json' \
+// --data-raw '{
+//     "name": "",
+//     "description": "This is your first repository",
+//     "homepage": "https://github.com",
+//     "private": false,
+//     "has_issues": true,
+//     "has_projects": true,
+//     "has_wiki": true
+// }'
+
+// api to delete repo)
+// curl --location --request DELETE 'https://api.github.com/repos/PUT YOUR USERNAME HERE/REPO_NAME'
+
+
+int main() {
+    bool init = false;
+    if (init) system("git init");
+
+    // adding all changes
+    system("git add .");
+    char message[1024];
+
+    // get message from user
+    printf("Enter commit message: ");
+    fgets(message ,1024, stdin);
+
+    // remove '\n' from fgets()
+    message[strcspn(message, "\n")] = 0;
+
+    // concatenating string for command
+    strcat(message, "'");
+    char command[1024] = "git commit -m '";
+    strcat(command, message);
+
+    // commiting and pushing to github and clearing terminal output
+    system(command);
+    system("git push -u origin main");
+    system("clear");
+    return 0;
+}
