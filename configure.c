d5d6d44619a3dd51f1e14b8d815a9224922a0738
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-10 11:16:22 -0400

    added basic structure for configure.c

--- configure.c
+++ configure.c
@@ -5,5 +5,11 @@
 
 int main() {
     system("git init");
+    system("git add .");
     
+    // commit();
+
+    system("git branch -M main");
+    // system("git remote add origin https://github.com/USER_HERE/REPO_NAME.git");
+    system("git push -u origin main");
 }
