3e3c2bd7fb01060b210bcc2642a1cdeb3a00e8a3
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-11 17:00:44 -0400

    added conditionals depending on os for initial configuration program

--- git_automator/configure.c
+++ git_automator/configure.c
@@ -5,12 +5,16 @@
 #include "main.c"
 
 int main() {
-    system("git init");
-    system("git add .");
-    
-    commit();
+    #ifdef __APPLE__
+        system("git init");
+        system("git add .");
+        
+        commit();
 
-    system("git branch -M main");
-    // system("git remote add origin https://github.com/USER_HERE/REPO_NAME.git");
-    system("git push -u origin main");
+        system("git branch -M main");
+        // system("git remote add origin https://github.com/USER_HERE/REPO_NAME.git");
+        system("git push -u origin main");
+    #elif __LINUX__
+    #elif _WIN32
+    #endif
 }
