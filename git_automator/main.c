e9946eccb0aa37175f000158e0fa61f9a864217c
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-11 12:07:39 -0400

    added functionality for ' quotes

--- git_automator/main.c
+++ git_automator/main.c
@@ -15,8 +15,8 @@ void commit() {
     message[strcspn(message, "\n")] = 0;
 
     // concatenating string for command
-    strcat(message, "'");
-    char command[1024] = "git commit -m '";
+    strcat(message, "\"");
+    char command[1024] = "git commit -m \"";
     strcat(command, message);
 
     // commiting command
