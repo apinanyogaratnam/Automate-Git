561f0d409e8df5650e7c43ceb4f9f6afd714d2ee
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-12 21:30:02 -0400

    removed comments for initial commit

--- git_automator/initial_configuration.c
+++ git_automator/initial_configuration.c
@@ -4,20 +4,15 @@
 #include <string.h>
 #include<unistd.h>
 
-// in initial_config dir
-// cloned project
-// in git_automator
 int init_config() {
     chdir("..");
-    // now in directory initial_config
+
 
     // move readme inside git_automator directory
     system("mv README.md git_automator/");
 
     // move git_automator to previous dir
     system("mv git_automator ..");
-
-    // there now exists initial_config and git_automator side to side
 
     return 0;
 }
