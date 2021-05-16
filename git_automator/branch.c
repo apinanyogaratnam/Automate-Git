5d3e6efaa77afe81e3e41a49f9aac675c6f1853e
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-15 20:40:37 -0400

    deleted a.out file

--- git_automator/branch.c
+++ git_automator/branch.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
     char push[1024] = "git push -u origin ";
     strcat(push, argv[1]);
     system(push);
-
+ 
     // to combine both git branch and git checkout we can use the command:
     // git checkout -b BRANCH_NAME
     return 0;
